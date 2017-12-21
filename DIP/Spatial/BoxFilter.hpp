21977f9270f1f1f3c1f1a96e520cf86a753c92c2
Jiang XueQian <jiangxueqian@gmail.com>
2017-12-21 09:07:46 +0800

    Rewrite box filter
    use template to clearify code

--- DIP/Spatial/BoxFilter.hpp
+++ DIP/Spatial/BoxFilter.hpp
@@ -13,8 +13,14 @@ namespace DIP {
 	namespace Spatial {
 		using namespace std;
 
-		inline Scalar
-		inAKUL(
+		template<
+				bool usan = false,
+				bool dsan = false,
+				bool lsan = false,
+				bool rsan = false
+			>
+		F_SHORT Scalar
+		boxInMajor_kApply(
 				ScalarArray &in,
 				ScalarArray &kernel,
 				Index x,
@@ -28,219 +34,15 @@ namespace DIP {
 			for (Index i = 0; i < kernel.rows(); i++)
 				for (Index j = 0; j < kernel.cols(); j++) {
 					Index inr, inc;
-					if (y + i < krh) {
+					inr = y - krh + i;
+					inc = x - kch + j;
+					if (usan && inr < 0)
 						inr = 0;
-					} else {
-						inr = y - krh + i;
-					};
-					if (x + j < kch) {
-						inc = 0;
-					} else {
-						inc = x - kch + j;
-					};
-					ret += kernel(i, j) * in(inr, inc);
-				};
-			return ret;
-		}
-
-		inline Scalar
-		inAKUM(
-				ScalarArray &in,
-				ScalarArray &kernel,
-				Index x,
-				Index y
-				)
-		{
-			Index krh, kch;
-			krh = kernel.rows() / 2;
-			kch = kernel.cols() / 2;
-			Scalar ret = 0;
-			for (Index i = 0; i < kernel.rows(); i++)
-				for (Index j = 0; j < kernel.cols(); j++) {
-					Index inr, inc;
-					if (y + i < krh) {
-						inr = 0;
-					} else {
-						inr = y - krh + i;
-					};
-					inc = x - kch + j;
-					ret += kernel(i, j) * in(inr, inc);
-				};
-			return ret;
-		}
-
-		inline Scalar
-		inAKUR(
-				ScalarArray &in,
-				ScalarArray &kernel,
-				Index x,
-				Index y
-				)
-		{
-			Index krh, kch;
-			krh = kernel.rows() / 2;
-			kch = kernel.cols() / 2;
-			Scalar ret = 0;
-			for (Index i = 0; i < kernel.rows(); i++)
-				for (Index j = 0; j < kernel.cols(); j++) {
-					Index inr, inc;
-					if (y + i < krh) {
-						inr = 0;
-					} else {
-						inr = y - krh + i;
-					};
-					inc = x - kch + j;
-					if (inc >= in.cols())
-						inc = in.cols() - 1;
-					ret += kernel(i, j) * in(inr, inc);
-				};
-			return ret;
-		}
-
-		inline Scalar
-		inAKML(
-				ScalarArray &in,
-				ScalarArray &kernel,
-				Index x,
-				Index y
-				)
-		{
-			Index krh, kch;
-			krh = kernel.rows() / 2;
-			kch = kernel.cols() / 2;
-			Scalar ret = 0;
-			for (Index i = 0; i < kernel.rows(); i++)
-				for (Index j = 0; j < kernel.cols(); j++) {
-					Index inr, inc;
-					inr = y - krh + i;
-					if (x + j < kch) {
-						inc = 0;
-					} else {
-						inc = x - kch + j;
-					};
-					ret += kernel(i, j) * in(inr, inc);
-				};
-			return ret;
-		}
-
-		inline Scalar
-		inAKMM(
-				ScalarArray &in,
-				ScalarArray &kernel,
-				Index x,
-				Index y
-				)
-		{
-			Index krh, kch;
-			krh = kernel.rows() / 2;
-			kch = kernel.cols() / 2;
-			Scalar ret = 0;
-			for (Index i = 0; i < kernel.rows(); i++)
-				for (Index j = 0; j < kernel.cols(); j++) {
-					Index inr, inc;
-					inr = y - krh + i;
-					inc = x - kch + j;
-					ret += kernel(i, j) * in(inr, inc);
-				};
-			return ret;
-		}
-
-		inline Scalar
-		inAKMR(
-				ScalarArray &in,
-				ScalarArray &kernel,
-				Index x,
-				Index y
-				)
-		{
-			Index krh, kch;
-			krh = kernel.rows() / 2;
-			kch = kernel.cols() / 2;
-			Scalar ret = 0;
-			for (Index i = 0; i < kernel.rows(); i++)
-				for (Index j = 0; j < kernel.cols(); j++) {
-					Index inr, inc;
-					inr = y - krh + i;
-					inc = x - kch + j;
-					if (inc >= in.cols())
-						inc = in.cols() - 1;
-					ret += kernel(i, j) * in(inr, inc);
-				};
-			return ret;
-		}
-
-		inline Scalar
-		inAKDL(
-				ScalarArray &in,
-				ScalarArray &kernel,
-				Index x,
-				Index y
-				)
-		{
-			Index krh, kch;
-			krh = kernel.rows() / 2;
-			kch = kernel.cols() / 2;
-			Scalar ret = 0;
-			for (Index i = 0; i < kernel.rows(); i++)
-				for (Index j = 0; j < kernel.cols(); j++) {
-					Index inr, inc;
-					inr = y - krh + i;
-					if (inr >= in.rows())
+					if (dsan && inr >= in.rows())
 						inr = in.rows() - 1;
-					if (x + j < kch) {
+					if (lsan && inc < 0)
 						inc = 0;
-					} else {
-						inc = x - kch + j;
-					};
-					ret += kernel(i, j) * in(inr, inc);
-				};
-			return ret;
-		}
-
-		inline Scalar
-		inAKDM(
-				ScalarArray &in,
-				ScalarArray &kernel,
-				Index x,
-				Index y
-				)
-		{
-			Index krh, kch;
-			krh = kernel.rows() / 2;
-			kch = kernel.cols() / 2;
-			Scalar ret = 0;
-			for (Index i = 0; i < kernel.rows(); i++)
-				for (Index j = 0; j < kernel.cols(); j++) {
-					Index inr, inc;
-					inr = y - krh + i;
-					if (inr >= in.rows())
-						inr = in.rows() - 1;
-					inc = x - kch + j;
-					ret += kernel(i, j) * in(inr, inc);
-				};
-			return ret;
-		}
-
-		inline Scalar
-		inAKDR(
-				ScalarArray &in,
-				ScalarArray &kernel,
-				Index x,
-				Index y
-				)
-		{
-			Index krh, kch;
-			krh = kernel.rows() / 2;
-			kch = kernel.cols() / 2;
-			Scalar ret = 0;
-			for (Index i = 0; i < kernel.rows(); i++)
-				for (Index j = 0; j < kernel.cols(); j++) {
-					Index inr, inc;
-					inr = y - krh + i;
-					if (inr >= in.rows())
-						inr = in.rows() - 1;
-					inc = x - kch + j;
-					if (inc >= in.cols())
+					if (rsan && inc >= in.cols())
 						inc = in.cols() - 1;
 					ret += kernel(i, j) * in(inr, inc);
 				};
@@ -260,29 +62,47 @@ namespace DIP {
 			for (; y < krh; y++) {
 				Index x = 0;
 				for (; x < kch; x++)
-					ret(y, x) = inAKUL(in, kernel, x, y);
+					ret(y, x) = boxInMajor_kApply<
+						true, false, true, false
+						>(in, kernel, x, y);
 				for (; x < ic - kch; x++)
-					ret(y, x) = inAKUM(in, kernel, x, y);
+					ret(y, x) = boxInMajor_kApply<
+						true, false, false, false
+						>(in, kernel, x, y);
 				for (; x < ic; x++)
-					ret(y, x) = inAKUR(in, kernel, x, y);
+					ret(y, x) = boxInMajor_kApply<
+						true, false, false, true
+						>(in, kernel, x, y);
 			};
 			for (; y < ir - krh; y++) {
 				Index x = 0;
 				for (; x < kch; x++)
-					ret(y, x) = inAKML(in, kernel, x, y);
+					ret(y, x) = boxInMajor_kApply<
+						false, false, true, false
+						>(in, kernel, x, y);
 				for (; x < ic - kch; x++)
-					ret(y, x) = inAKMM(in, kernel, x, y);
+					ret(y, x) = boxInMajor_kApply<
+						false, false, false, false
+						>(in, kernel, x, y);
 				for (; x < ic; x++)
-					ret(y, x) = inAKMR(in, kernel, x, y);
+					ret(y, x) = boxInMajor_kApply<
+						false, false, false, true
+						>(in, kernel, x, y);
 			};
 			for (; y < ir; y++) {
 				Index x = 0;
 				for (; x < kch; x++)
-					ret(y, x) = inAKDL(in, kernel, x, y);
+					ret(y, x) = boxInMajor_kApply<
+						false, true, true, false
+						>(in, kernel, x, y);
 				for (; x < ic - kch; x++)
-					ret(y, x) = inAKDM(in, kernel, x, y);
+					ret(y, x) = boxInMajor_kApply<
+						false, true, false, false
+						>(in, kernel, x, y);
 				for (; x < ic; x++)
-					ret(y, x) = inAKDR(in, kernel, x, y);
+					ret(y, x) = boxInMajor_kApply<
+						false, true, false, true
+						>(in, kernel, x, y);
 			};
 			return ret;
 		}
